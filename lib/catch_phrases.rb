068d836992389eb844c02d469882eb393a4e44b2
Travis Prol <travisjohnprol@gmail.com>
2020-05-01 21:03:32 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,8 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  words = "It's Dangerous To Go Alone! Take this."
+  puts words
+end
